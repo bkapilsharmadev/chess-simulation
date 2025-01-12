47ab473cd32d4dfebdf7e8a76fb4120cbc990403
Kapil Sharma <bkapilsharma.dev@gmail.com>
2025-01-12 16:46:19 +0530

    added test cases for ChessPiece.isValidPiece()

--- test/chess-piece.test.js
+++ test/chess-piece.test.js
@@ -36,7 +36,27 @@ describe('ChessPiece', () => {
         test('should throw error if pieceClass is not an instance of ChessPiece', () => {
             expect(() => ChessPiece.registerPiece('Bishop', class { })).toThrow();
         });
+
+        test('should throw error if pieceClass is not an instance of ChessPiece', () => {
+            expect(() => ChessPiece.registerPiece('Dragon', null)).toThrow();
+        });
     });
+
+    describe('isValidPiece()', () => {
+        test('should return true if piece is registered', () => {
+            expect(ChessPiece.isValidPiece('Pawn')).toBe(true);
+            expect(ChessPiece.isValidPiece('King')).toBe(true);
+            expect(ChessPiece.isValidPiece('Queen')).toBe(true);
+        });
+
+        test('should return false if piece is not registered', () => {
+            expect(ChessPiece.isValidPiece('Knight')).toBe(false);
+            expect(ChessPiece.isValidPiece(null)).toBe(false);
+            expect(ChessPiece.isValidPiece('')).toBe(false);
+            expect(ChessPiece.isValidPiece(undefined)).toBe(false);
+        });
+    });
+
 
 
 
